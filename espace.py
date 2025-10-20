8c9e2c095a02416d1888eb9238cf3a7aa5e65a85
you-mes <youcefmessaoudcity45@gmail.com>
2025-10-20 19:51:38 +0000

    code source modifié
    Enabled grid display for age distribution plot.

--- espace.py
+++ espace.py
@@ -11,19 +11,8 @@ plott.hist(age, bins=100, color='blue', edgecolor='black')
 plott.title('age distribution of your customers')
 plott.xlabel('age')
 plott.ylabel('frequency')
-#plott.grid(True)
+plott.grid(True)
 
 plott.show()
 
-categories = ['Electronics', 'Furniture', 'Clothing', 'Food', 'Toys']
-category_sales = [1500, 1200, 950, 1800, 700]
 
-color_multiple=['red', 'green', 'blue', 'yellow', 'violet']
-plott.bar(categories, category_sales,  color=color_multiple)
-
-plott.title('sales by produt categoriy')
-plott.xlabel('category')
-plott.ylabel('total sales(s)')
-plott.grid(True)
-plott.legend('barchar')
-#plott.show()
